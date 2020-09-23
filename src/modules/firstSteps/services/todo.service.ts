262074c8921e135725bd4ba86b1bc00d1c24ac66
david-373 <71123758+david-373@users.noreply.github.com>
2020-09-23 18:40:01 +0400

    first backend api

--- src/modules/firstSteps/services/todo.service.ts
+++ src/modules/firstSteps/services/todo.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
-import { Repository } from 'typeorm';
 import { Todo } from '../entities/todo.entity';
 
 @Injectable()
@@ -10,23 +9,5 @@ export class TodoService extends TypeOrmCrudService<Todo> {
         super(todoRepository)
     }
 
-    findAll(): Promise<Todo[]> {
-        return this.todoRepository.find();
-    }
 
-    findOne(id: string): Promise<Todo> {
-        return this.todoRepository.findOne(id);
-    }
-
-    create(todo: Todo): Promise<Todo> {
-        delete todo.id
-        return this.todoRepository.save(todo);
-    }
-    update(todo: Todo): Promise<Todo> {
-        return this.todoRepository.save(todo);
-    }
-
-    async remove(id: string): Promise<void> {
-        await this.todoRepository.delete(id);
-    }
 }
